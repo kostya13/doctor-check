143e4b9aefaa436bd7cb2399adb76ca64c97cf84
Konstantin Ilyashenko <konstantin@cloudify.co>
2018-01-25 14:34:00 +0400

    Исправлен скрипт проверки

--- doctor_check/watch.py
+++ doctor_check/watch.py
@@ -58,26 +58,29 @@ def main():
         for subs in subscriptions:
             if not subscriptions[subs]:
                 continue
-            hosp_id = subscriptions[subs][0][1].split('&')[0]
+            hosp_id = subscriptions[subs].keys()[0].split('&')[0]
             data = requests.get(
                 'http://igis.ru/online{0}&page=zapdoc'.format(hosp_id))
             if not data.ok:
+                logger.error("Ошибка загрузки: {0}".format(data.text))
                 break
-            docs = set([doc[1] for doc in subscriptions[subs]])
+            docs = subscriptions[subs].keys()
             soup = BeautifulSoup(data.text, 'html.parser')
             for c in soup.find_all('table')[5].children:
-                if 'Всего номерков' in str(c):
-                    href = c.find_all('a')[1].attrs['href']
-                    if href in docs:
-                        logger.debug("Найдено совпадение: {0}".format(href))
-                        cleanup.append(href)
-                        for email in [d[2] for d in subscriptions[subs] if d[1] == href]:
-                            send_email(email, 'http://igis.ru/online{0}'.format(href))
+                if 'Всего номерков' not in str(c):
+                    continue
+                href = c.find_all('a')[1].attrs['href']
+                if href in docs:
+                    logger.debug("Найдено совпадение: {0}".format(href))
+                    cleanup.append(href)
+                    for email in subscriptions[subs][href]['emails']:
+                        send_email(email,
+                                   'http://igis.ru/online{0}'.format(href))
         if cleanup:
             for hospital in subscriptions:
-                current = [d for d in subscriptions.setdefault(hospital, [])
-                           if d[1] not in cleanup]
-                subscriptions[hospital] = current
+                for doc_id in cleanup:
+                    if doc_id in subscriptions[hospital].keys():
+                        del subscriptions[hospital][doc_id]
             with codecs.open(full_path(), 'w', encoding="utf-8") as f:
                 json.dump(subscriptions, f, ensure_ascii=False,
                           encoding='utf-8')
